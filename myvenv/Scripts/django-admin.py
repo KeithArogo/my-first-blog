eb478bd624a2c74ad324770c88e0f08f081cf824
KeithArogo <Keithowino97@gmail.com>
2020-02-21 14:00:50 +0800

    My Django Girls app, first commit

---
+++ myvenv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\user\desktop\project_practise_project\django_girls\myvenv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
