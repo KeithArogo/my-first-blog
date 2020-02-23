f43ab5ed2a5349ad10a83a52569e7cdd913332f5
KeithArogo <Keithowino97@gmail.com>
2020-02-23 23:15:14 +0800

    Modified templets to display posts from Database

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
-
+from django.utils import timezone
+from .models import Post#The dot before models means it is in the current directtory
 # Create your views here.
-
+# request is anything you receive from the user via the internet
 def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'blog/post_list.html', {'posts': posts})
